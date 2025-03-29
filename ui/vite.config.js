601236ae0b28fda6345cc8f33c6b48af297a3e4b
unknown <robertpierce2828@gmail.com>
2025-03-29 12:49:37 +0800

    first commit

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // 添加 React 插件
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  base: '/HuangQi_model/',
   plugins: [
-    react(), // 添加这一行
     tailwindcss(),
   ],
 })
